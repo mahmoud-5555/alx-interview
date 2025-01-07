25d46807d104cb8e80c14ac6911b190c93a637ca
mahmoud-5555 <mahmoudadam5555@gmail.com>
2025-01-07 05:11:10 +0200

    test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,7 +3,7 @@
 
 
 def is_winner(x, nums):
-    """ Determines the winner of the prime game."""
+    """Function to Determines the winner of the prime game."""
     prime_numbers = [
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
         53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
