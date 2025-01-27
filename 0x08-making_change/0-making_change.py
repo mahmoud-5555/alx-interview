05fbb21f36abbf48b3f8bdcc8f04a8e3e0e5531b
mahmoud-5555 <mahmoudadam5555@gmail.com>
2025-01-27 18:03:29 +0200

    0-making_change.py test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,10 +10,10 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0  # base case
-
+    dp = [0]
+    max_value = total + 1
     for i in range(1, total + 1):
+        dp.append(max_value)
         for coin in coins:
             if i >= coin:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
