e8c59b092c442d5a47787c91387f5424780440d2
mahmoud-5555 <mahmoudadam5555@gmail.com>
2025-01-27 15:02:06 +0200

    0-making_change.py test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,13 @@ def makeChange(coins, total):
     Returns the minimum number of coins needed to
       make change for the given total.
       """
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0  # base case
-
     if total == 0:
         return 0
+    if total < 0:
+        return -1
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0  # base case
 
     for i in range(1, total + 1):
         for coin in coins:
