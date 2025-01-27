5b7b49333d61b22d5b3f194d11db487c7aa7c4b6
mahmoud-5555 <mahmoudadam5555@gmail.com>
2025-01-27 12:31:50 +0200

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,24 +8,21 @@ def island_perimeter(grid):
         for index_y, element in enumerate(row):
             if element == 1:
                 result += 4
-        
+
                 if index_x > 0:
                     if grid[index_x - 1][index_y] == 1:
                         result -= 1
-        
+
                 if index_x < len(grid) - 1:
                     if grid[index_x + 1][index_y] == 1:
                         result -= 1
 
-                if index_y > 0:        
+                if index_y > 0:
                     if row[index_y - 1] == 1:
                         result -= 1
 
-                if index_y < len(row) - 1:        
+                if index_y < len(row) - 1:
                     if row[index_y + 1] == 1:
                         result -= 1
-        
+
     return result
-
-
-              
