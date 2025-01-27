9ccb97faa95a89e3d8fc3d1caa8e67ef240e41e7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2025-01-27 14:54:42 +0200

    0-making_change.py test

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module is a solution for the Making Change problem. """
+
+
+def makeChange(coins, total):
+    """
+    Returns the minimum number of coins needed to
+      make change for the given total.
+      """
+    dp = [total + 1] * total + 1
+    dp[0] = 0  # base case
+
+    if total == 0:
+        return 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != total + 1 else -1
